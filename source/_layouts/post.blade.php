f0ca25b26447c6030f13d5a72e2450251089ff6f
Marius White <arius@Marius-MacBook-Pro.local>
2020-04-04 15:42:44 +0100

    fix images

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -40,7 +40,7 @@
     </div>
 
     <nav class="flex flex-col md:flex-row justify-between text-sm text-dark mb-4">
-        <div class="w-full md:w-1/2 mb-3">
+        <div class="w-full md:w-1/2 mb-3 md:mr-2">
             @if ($next = $page->getNext())
                 <a
                     href="{{ $next->getUrl() }}"
