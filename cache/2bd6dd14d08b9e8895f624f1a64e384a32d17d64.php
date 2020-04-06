fd4f83573fb7c8be51d8368c593ba3bedd1a7e8b
Marius White <arius@Marius-MacBook-Pro.local>
2020-04-06 20:05:19 +0100

    wip

--- cache/2bd6dd14d08b9e8895f624f1a64e384a32d17d64.php
+++
@@ -1,6 +0,0 @@
-<div class="p-4 lg:p-0 flex items-baseline justify-between border-b border-primary">
-	<h4 class="text-dark font-normal text-sm flex items-center">
-		<?php echo e($breadcrumb); ?>
-
-	</h4>
-</div><?php /**PATH /Users/nate/Documents/websites/finescoop/source/_layouts/partials/breadcrumb.blade.php ENDPATH**/ ?>
