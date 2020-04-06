80529e1554f20fd9ae2a4131cf1eab815764d125
Marius White <arius@Marius-MacBook-Pro.local>
2020-04-06 16:13:51 +0100

    fix images

--- source/_layouts/components/post-preview-inline.blade.php
+++ source/_layouts/components/post-preview-inline.blade.php
@@ -11,10 +11,10 @@
         </div>
         <div class="border border-primary bg-white flex flex-col justify-between leading-normal">
             <div class="px-4 py-2 mb-2">
-                <div class="text-darker leading-snug font-semibold text-lg mb-4">
+                <div class="text-darker leading-snug font-semibold text-lg mb-4 article-title">
                     {{ $post->title }}
                 </div>
-                <p class="text-light font-normal text-sm">
+                <p class="text-light font-normal text-sm article-description">
                     {!! $post->description !!}
                 </p>
             </div>
--- tools/twitter/Find.php
+++ tools/twitter/Find.php
@@ -5,9 +5,17 @@ namespace Tools\Twitter;
 use DOMDocument;
 use DOMXPath;
 use Tools\Twitter\Post;
+use function var_dump;
 
 class Find
 {
+    /**
+     * Front page with latest articles
+     *
+     * @var string
+     */
+    private $uri = 'http://finescoop.com';
+
     /**
      * @var \Tools\Twitter\Post
      */
@@ -21,19 +29,58 @@ class Find
         $this->post = new Post();
     }
 
-    public function getArticles()
+    /**
+     * Run the tool
+     */
+    public function run()
     {
-        $dom = new \DOMDocument('http://finescoop.com');
-        $xpath = new DOMXPath($dom);
+        $articles = $this->getArticles();
 
-        $items = $xpath->query('div.article-post');
-        var_dump($items);
+        var_dump($articles);
+    }
+
+    /**
+     * Get all the articles
+     *
+     * @return \DOMNodeList|false
+     */
+    private function getArticles()
+    {
+        // Get the content
+        $dom = $this->getContent($this->uri);
+
+        $xpath = new \DOMXPath($dom);
+
+        // Loops through items, and create array of articles
+        $items = $xpath->query("//*[@class=\"article-post\"]");
+        foreach($items as $item) {
+            $title = $xpath->query("descendant::*[@class='article-title']", $item);
+            $description = $xpath->query("descendant::*[@class='article-description']", $item);
+            var_dump($title);
+        }
+
+        return [
+            'title' => $title,
+            '$description' => $description,
+        ];
+    }
+
+    /**
+     * Get the html of the website
+     *
+     * @param string $uri
+     * @return \DOMDocument
+     */
+    private function getContent(string $uri)
+    {
+        $dom = new \DOMDocument();
+        $dom->loadHTMLFile($uri);
+
+        return $dom;
     }
 }
-
-
 
 // Autoload scrape classes
 require __DIR__ . '/../../vendor/autoload.php';
 
-(new Find)->getArticles();
+(new Find)->run();
