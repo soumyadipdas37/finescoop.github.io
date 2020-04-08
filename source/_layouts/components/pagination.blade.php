c5a1678897d22c1478f2e335fe74f0374ec7979b
Marius White <arius@Marius-MacBook-Pro.local>
2020-04-08 14:48:37 +0100

    wip

--- source/_layouts/components/pagination.blade.php
+++ source/_layouts/components/pagination.blade.php
@@ -2,7 +2,7 @@
 	<nav class="flex text-base my-8 px-2 md:px-0 pagination">
 		@if ($previous = $previousPage)
 			<a
-				href="{{ $page->baseUrl() }}/{{ $previous }}"
+				href="{{ $page->baseUrl }}/{{ $previous }}"
 				title="Previous Page"
 				class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
 			>&LeftArrow;</a>
@@ -10,7 +10,7 @@
 
 		@foreach ($pages as $pageNumber => $path)
 			<a
-				href="{{ $page->baseUrl() }}/{{ $path }}"
+				href="{{ $page->baseUrl }}/{{ $path }}"
 				title="Go to Page {{ $pageNumber }}"
 				class="bg-gray-200 hover:bg-gray-400 text-blue-700 rounded mr-3 px-5 py-3 {{ $currentPage == $pageNumber ? 'text-blue-600' : '' }}"
 			>{{ $pageNumber }}</a>
@@ -18,7 +18,7 @@
 
 		@if ($next = $nextPage)
 			<a
-				href="{{ $page->baseUrl() }}/{{ $next }}"
+				href="{{ $page->baseUrl }}/{{ $next }}"
 				title="Next Page"
 				class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
 			>&RightArrow;</a>
