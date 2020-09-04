11f3641aa6268eafdcfa36fec10048c192596a52
Nate Corkish <66919946+natecorkish@users.noreply.github.com>
2020-09-04 20:03:53 +0100

    Update post-as-rss-item.blade.php

--- source/_layouts/components/post-as-rss-item.blade.php
+++ source/_layouts/components/post-as-rss-item.blade.php
@@ -1,17 +1,1 @@
-<entry>
-    <id>{{ $entry->getUrl() }}</id>
-    <link type="text/html" rel="alternate" href="{{ $entry->getUrl() }}" />
-    <title>{{ $entry->title ?? '' }}</title>
-    <?php
-        $time = DateTime::createFromFormat('Y-m-d-H-i-s', $entry->date);
-    ?>
-    <published>{{ $time->format(DateTime::ATOM) }}</published>
-    <updated>{{ $time->format(DateTime::ATOM) }}</updated>
-    <author>
-        <name>{{ $entry->author }}</name>
-    </author>
-    <summary type="html">{{ $entry->description }}...</summary>
-    <content type="html"><![CDATA[
-        @includeFirst(['_posts.' . $entry->getFilename(), '_posts._tmp.' . $entry->getFilename()])
-    ]]></content>
-</entry>
+
