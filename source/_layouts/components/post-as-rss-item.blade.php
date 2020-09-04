eafd81bc1322d11a2c280c88a1fee797c2ada318
Nate Corkish <66919946+natecorkish@users.noreply.github.com>
2020-09-04 20:00:16 +0100

    Update post-as-rss-item.blade.php

--- source/_layouts/components/post-as-rss-item.blade.php
+++ source/_layouts/components/post-as-rss-item.blade.php
@@ -1,7 +1,7 @@
 <entry>
     <id>{{ $entry->getUrl() }}</id>
     <link type="text/html" rel="alternate" href="{{ $entry->getUrl() }}" />
-    <title>{{ $entry->title }}</title>p
+    <title>{{ $entry->title ?? '' }}</title>
     <?php
         $time = DateTime::createFromFormat('Y-m-d-H-i-s', $entry->date);
     ?>
