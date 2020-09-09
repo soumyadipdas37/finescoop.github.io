42ad29e828e75e800fcc80f360f305e7fba7ee79
Nate Corkish <66919946+natecorkish@users.noreply.github.com>
2020-09-09 13:18:59 +0100

    Update breadcrumb.blade.php

--- source/_layouts/partials/breadcrumb.blade.php
+++ source/_layouts/partials/breadcrumb.blade.php
@@ -1,5 +1,10 @@
-<div class="p-4 lg:p-0 flex items-baseline justify-between border-b border-primary">
-	<h4 class="text-dark font-normal text-sm flex items-center">
-		{{ $breadcrumb }}
-	</h4>
+<div class="p-4 lg:p-0 flex justify-between border-b border-primary">
+	<div class="w-2/3 flex items-center">
+		<h4 class="text-dark font-normal text-sm flex items-center">
+			{{ $breadcrumb }}
+		</h4>
+	</div>
+	<div class="w-1/3 flex items-center justify-end">
+		{{ $social ?? '' }}
+	</div>
 </div>
