656437b9019062b453a5415e2cf64e2c4fba1d20
Nate Corkish <66919946+natecorkish@users.noreply.github.com>
2020-09-26 19:02:22 +0100

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -34,14 +34,6 @@
                 data-ad-client="ca-pub-8880749208215381"
                 async
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162824010-1"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'UA-162824010-1');
-            </script>
         @endif
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
@@ -78,6 +70,14 @@
 
         </div>
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162824010-1"></script>
+        <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-162824010-1');
+        </script>
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
         @stack('scripts')
     </body>
